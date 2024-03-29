66b6a243c6f0e1dfd9e8ac935146bacef025e727
Yoon Kim <yoon.kim@tophatmonocle.com>
2024-03-29 15:12:38 -0300

    Added introduction

--- my-website/src/App.tsx
+++ my-website/src/App.tsx
@@ -1,10 +1,12 @@
 import NavBar from './Components/NavBar.js'
+import Introduction from './Components/Introduction.js'
 import './App.css'
 
 function App() {
   return (
     <div className="main">
         <NavBar />
+        <Introduction />
     </div>
   )
 }
---
+++ my-website/src/Components/Introduction.tsx
@@ -0,0 +1,15 @@
+import './IntroductionStyles.css'
+
+function Introduction() {
+    return (
+        <div className="introduction">
+            <h4>Hi, my name is</h4>
+            <h1 className="name">Yoon Kim</h1>
+            <h1 className="role">I'm a Software Developer</h1>
+            <p className="description">I'm a software engineer with a passion for web development. I have experience with React, Node.js,
+                and Python. I'm currently looking for new opportunities to grow and learn.</p>
+        </div>
+    )
+}
+
+export default Introduction
