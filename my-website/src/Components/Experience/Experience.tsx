f6d782643abc4200bac48d9322be92b088090660
Yoon Kim <yoon.kim@tophatmonocle.com>
2024-04-03 23:58:25 -0300

    Added links to companies

--- my-website/src/Components/Experience/Experience.tsx
+++ my-website/src/Components/Experience/Experience.tsx
@@ -9,10 +9,10 @@ function Experience () {
                 <div className="exp">
                     <div className="expContainer">
                         <h3 className="date">Jan 2024 - May 2024</h3>
-                        <h2 className="company">Top Hat</h2>
+                        <h2 className="company"><a href="https://tophat.com/" target="_blank">Top Hat</a></h2>
                         <h3 className="location">Toronto, ON</h3>
+                        <h4 className="role">Software Engineer</h4>
                         <div className="description">
-                            <h4>Software Engineer</h4>
                             <p>• Migrated over 500 unit tests from an Enzyme Library to a React Testing Library for easier maintainability, and
                                 better support for user interactions, resulting in a 4% decrease in errors caught by Sentry.</p>
                             <p>• Fixed 20+ critical bugs by utilizing thorough debugging techniques and collaborating with cross-functional teams,
@@ -26,10 +26,10 @@ function Experience () {
                 <div className="exp">
                     <div className="expContainer">
                         <h3 className="date">May 2023 - Sept 2023</h3>
-                        <h2 className="company">VitaShop</h2>
+                        <h2 className="company"><a href="https://vitashop.ca/en/" target="_blank">VitaShop</a></h2>
                         <h3 className="location">Toronto, ON</h3>
+                        <h4 className="role">Digital Marketing / Frontend Developer</h4>
                         <div className="description">
-                            <h4>Digital Marketing / Frontend Developer</h4>
                             <p>• Refined the website’s interactivity and categorization of products using, PHP, HTML, and CSS, improving
                                 customer’s ease of use, resulting in 5-figures in increased earnings.</p>
                             <p>• Integrated advanced tracking solutions and streamline analytic processes through Google Analytics to capture user
@@ -43,10 +43,10 @@ function Experience () {
                 <div className="exp">
                     <div className="expContainer">
                         <h3 className="date">June 2022 - Sept 2022</h3>
-                        <h2 className="company">Cooke Aquaculture</h2>
+                        <h2 className="company"><a href="https://aquaculturegrowsns.ca/" target="_blank">Cooke Aquaculture</a></h2>
                         <h3 className="location">Saint John, NB</h3>
+                        <h4 className="role">Software Developer Intern</h4>
                         <div className="description">
-                            <h4>Software Developer Intern</h4>
                             <p>• Engineered a Python script to optimize the organization of the company’s active directory, managing over 4000
                                 users. The solution significantly enhanced the directory structure, leading to a notable 25% increase in operational
                                 efficiency.</p>
