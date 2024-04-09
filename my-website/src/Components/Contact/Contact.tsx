385fb1bd7d87391444df290bf0558fbd940bb206
Yoon Kim <yoon.kim@uwaterloo.ca>
2024-04-09 00:22:17 -0300

    update

--- my-website/src/Components/Contact/Contact.tsx
+++ my-website/src/Components/Contact/Contact.tsx
@@ -2,53 +2,55 @@ import './ContactStyles.css'
 
 function Contact () {
     return (
-        <div className="background">
-            <div className="container">
-                <div className="screen">
-                    <div className="screen-header">
-                        <div className="screen-header-left">
-                            <div className="screen-header-button close"></div>
-                            <div className="screen-header-button maximize"></div>
-                            <div className="screen-header-button minimize"></div>
-                        </div>
-                        <div className="screen-header-right">
-                            <div className="screen-header-ellipsis"></div>
-                            <div className="screen-header-ellipsis"></div>
-                            <div className="screen-header-ellipsis"></div>
-                        </div>
-                    </div>
-                    <div className="screen-body">
-                        <div className="screen-body-item left">
-                            <div className="app-title">
-                                <span>CONTACT</span>
-                                <span>US</span>
+        <section id = "contact">
+            <div className="background">
+                <div className="container">
+                    <div className="screen">
+                        <div className="screen-header">
+                            <div className="screen-header-left">
+                                <div className="screen-header-button close"></div>
+                                <div className="screen-header-button maximize"></div>
+                                <div className="screen-header-button minimize"></div>
                             </div>
-                            <div className="app-contact">CONTACT INFO : 1 (506) 285-6710</div>
+                            <div className="screen-header-right">
+                                <div className="screen-header-ellipsis"></div>
+                                <div className="screen-header-ellipsis"></div>
+                                <div className="screen-header-ellipsis"></div>
+                            </div>
                         </div>
-                        <div className="screen-body-item">
-                            <div className="app-form">
-                                <div className="app-form-group">
-                                    <input className="app-form-control" placeholder="NAME" value="Yoon Kim"/>
+                        <div className="screen-body">
+                            <div className="screen-body-item left">
+                                <div className="app-title">
+                                    <span>CONTACT</span>
+                                    <span>ME</span>
                                 </div>
-                                <div className="app-form-group">
-                                    <input className="app-form-control" placeholder="EMAIL"/>
-                                </div>
-                                <div className="app-form-group">
-                                    <input className="app-form-control" placeholder="CONTACT NO"/>
-                                </div>
-                                <div className="app-form-group message">
-                                    <input className="app-form-control" placeholder="MESSAGE"/>
-                                </div>
-                                <div className="app-form-group buttons">
-                                    <button className="app-form-button">CANCEL</button>
-                                    <button className="app-form-button">SEND</button>
+                                <div className="app-contact">CONTACT INFO : 1 (506) 285-6710</div>
+                            </div>
+                            <div className="screen-body-item">
+                                <div className="app-form">
+                                    <div className="app-form-group">
+                                        <input className="app-form-control" placeholder="NAME"/>
+                                    </div>
+                                    <div className="app-form-group">
+                                        <input className="app-form-control" placeholder="EMAIL"/>
+                                    </div>
+                                    <div className="app-form-group">
+                                        <input className="app-form-control" placeholder="CONTACT NO"/>
+                                    </div>
+                                    <div className="app-form-group message">
+                                        <input className="app-form-control" placeholder="MESSAGE"/>
+                                    </div>
+                                    <div className="app-form-group buttons">
+                                        <button className="app-form-button">CANCEL</button>
+                                        <button className="app-form-button">SEND</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
