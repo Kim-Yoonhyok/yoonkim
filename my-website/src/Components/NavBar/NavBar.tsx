0d973d54b8505cabd02cf11bdb319af3abeb50fd
Yoon Kim <yoon.kim@tophatmonocle.com>
2024-04-14 17:59:15 -0300

    navbar appear on scroll

--- my-website/src/Components/NavBar/NavBar.tsx
+++ my-website/src/Components/NavBar/NavBar.tsx
@@ -1,21 +1,49 @@
 import './NavBarStyles.css'
+import React, { useState, useEffect } from 'react'
 import logo from '../../Images/logo.png'
 
 function NavBar () {
-    return (
-        <nav className="navbar">
-            <img src={logo} alt='logo' className='logo'></img>
-            <ul className="nav-list">
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li>Projects</li>
-                <li><a href="#contact">Contact</a></li>
-                <a href="/resume.pdf" target="_blank">
-                    <button className="resume-button">Resume</button>
-                </a>
+    const [showNav, setShowNav] = useState(true)
+    let lastScrollTop = 0
 
-            </ul>
-        </nav>
+    useEffect(() => {
+        const handleScroll = () => {
+            let st = window.pageYOffset || document.documentElement.scrollTop
+            console.log(st, lastScrollTop)
+            if (st < lastScrollTop) {
+                console.log('show')
+                setShowNav(true)
+                console.log(showNav)
+            } else {
+                setShowNav(false)
+            }
+            lastScrollTop = st <= 0 ? 0 : st
+        }
+
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+
+
+    return (
+        //<div className={showNav ? 'show' : 'hide'}>
+            <nav className={"navbar"}>
+                <img src={logo} alt='logo' className='logo'></img>
+                <ul className="nav-list">
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li>Projects</li>
+                    <li><a href="#contact">Contact</a></li>
+                    <a href="/resume.pdf" target="_blank">
+                        <button className="resume-button">Resume</button>
+                    </a>
+
+                </ul>
+            </nav>
+        //</div>
     )
 }
 
